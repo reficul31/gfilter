a8e4e52a9ce93b97ed7c7e0d36d9fb76857db672
reficul31 <reficul31@gmail.com>
2019-02-26 12:02:03 +0530

    Adding method At and CalculateShift

--- errors.go
+++ errors.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	// ErrRowColumnCannotBeZero thrown when either row or columns or both is zero
 	ErrRowColumnCannotBeZero = errors.New("Row and Column cannot be zero")
+	// ErrRowColumnOutOfBounds is out of bounds for the defined kernel
+	ErrRowColumnOutOfBounds = errors.New("Row and/or Column is out of bounds")
 )
--- filter.go
+++ filter.go
@@ -2,13 +2,13 @@ package gfilter
 
 // Kernel struct handles data for the filter kernel
 type Kernel struct {
-	values  []uint32
-	rows    uint32
-	columns uint32
+	values  []float
+	Rows    uint32
+	Columns uint32
 }
 
 // CreateKernel creates a kernel object from the params
-func CreateKernel(values []uint32, rows, columns uint32) (*Kernel, error) {
+func CreateKernel(values []float, rows, columns uint32) (*Kernel, error) {
 	if rows == 0 || columns == 0 {
 		return nil, ErrRowColumnCannotBeZero
 	}
@@ -17,4 +17,18 @@ func CreateKernel(values []uint32, rows, columns uint32) (*Kernel, error) {
 		rows:    rows,
 		columns: columns,
 	}, nil
+}
+
+// At gets the value of the kernel at the specified row and column
+func (k *Kernel)At(row, column uint32) (float, error) {
+	if row >= k.Rows || column >= k.Column {
+		return 0.0, ErrRowColumnOutOfBounds
+	}
+
+	return k.Values[row*k.Columns + column], nil
+}
+
+// CalculateShift returns the amount the original image must be shifted
+func (k *Kernel)CalculateShift() (int, int) {
+	return int(k.Rows/2), int(k.Columns/2)
 }
