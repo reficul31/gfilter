fdc73534cae7d6bbe8ba35ab89ab70f9e29adaff
reficul31 <reficul31@gmail.com>
2019-03-08 12:27:34 +0530

    Removing naming errors

--- filter.go
+++ filter.go
@@ -2,30 +2,30 @@ package gfilter
 
 // Kernel struct handles data for the filter kernel
 type Kernel struct {
-	values  []float
+	values  []float32
 	Rows    uint32
 	Columns uint32
 }
 
 // CreateKernel creates a kernel object from the params
-func CreateKernel(values []float, rows, columns uint32) (*Kernel, error) {
+func CreateKernel(values []float32, rows, columns uint32) (*Kernel, error) {
 	if rows == 0 || columns == 0 {
 		return nil, ErrRowColumnCannotBeZero
 	}
 	return &Kernel{
 		values:  values,
-		rows:    rows,
-		columns: columns,
+		Rows:    rows,
+		Columns: columns,
 	}, nil
 }
 
 // At gets the value of the kernel at the specified row and column
-func (k *Kernel)At(row, column uint32) (float, error) {
-	if row >= k.Rows || column >= k.Column {
+func (k *Kernel)At(row, column uint32) (float32, error) {
+	if row >= k.Rows || column >= k.Columns {
 		return 0.0, ErrRowColumnOutOfBounds
 	}
 
-	return k.Values[row*k.Columns + column], nil
+	return k.values[row*k.Columns + column], nil
 }
 
 // CalculateShift returns the amount the original image must be shifted
