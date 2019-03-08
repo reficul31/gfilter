28384ae063aeb54456b6075b350553df9f994073
reficul31 <reficul31@gmail.com>
2019-03-08 12:26:59 +0530

    Renaming filter to kernel

--- convolution.go
+++ convolution.go
@@ -1,7 +1,6 @@
 package gfilter
 
 import (
-	"fmt"
 	"image"
 	"image/draw"
 )
@@ -9,6 +8,6 @@ import (
 // Apply is the convolution function used to apply the provided filter
 // on the given image. It makes a copy and returns the filtered image
 // without modifying the src image.
-func Apply(filter *Filter, src image.Image) (draw.Image, error) {
+func Apply(filter *Kernel, src image.Image) (draw.Image, error) {
 
 }
