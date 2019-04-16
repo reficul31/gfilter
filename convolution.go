11b25de72b57ff0c653a1adca970a4c7dcffddd5
reficul31 <reficul31@gmail.com>
2019-04-16 19:39:37 +0530

    Fixing args and setting up

--- convolution.go
+++ convolution.go
@@ -1,13 +1,12 @@
 package gfilter
 
-import (
-	"image"
-	"image/draw"
-)
-
 // Apply is the convolution function used to apply the provided filter
 // on the given image. It makes a copy and returns the filtered image
 // without modifying the src image.
-func Apply(filter *Kernel, src image.Image) (draw.Image, error) {
-
+func Apply(filter *Kernel, src ImageHandler) (ImageHandler, error) {
+	result, err := New(src.Mode(), src.GetDimensions())
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
