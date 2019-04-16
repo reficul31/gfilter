e6d7d125de7a9e9e94f9ba2212dcfffac8bbe59d
reficul31 <reficul31@gmail.com>
2019-04-16 19:39:20 +0530

    Adding Interface function GetDimensions and New

--- reader.go
+++ reader.go
@@ -1,14 +1,14 @@
 package gfilter
 
 import (
-	"os"
 	"image"
+	"os"
 	// but is imported for its initialization side-effect, which allows
 	// image.Decode to understand JPEG formatted images. Uncomment these
 	// two lines to also understand GIF and PNG images:
 	_ "image/gif"
-	_ "image/png"
 	_ "image/jpeg"
+	_ "image/png"
 )
 
 // ImageType to add support for different images
@@ -34,11 +34,12 @@ type ImageHandler interface {
 	At(row, column int) (Pixel, error)
 	Set(row, column int, px Pixel) error
 	Mode() ImageType
+	GetDimensions() image.Rectangle
 }
 
 // NRGBAImageHandler struct to handle NRGBA Image
 type NRGBAImageHandler struct {
-	Image *image.NRGBA
+	Image     *image.NRGBA
 	ImageType ImageType
 }
 
@@ -64,12 +65,17 @@ func (handler *NRGBAImageHandler) Set(row, column int, px Pixel) error {
 
 // Mode returns the ImageType for the image
 func (handler *NRGBAImageHandler) Mode() ImageType {
-	return handler.ImageType;
+	return handler.ImageType
 }
 
-// NRGBA64ImageHandler struct to handle NRGBA64 Image 
+// GetDimensions returns the dimensions as a Rectangle for the image
+func (handler *NRGBAImageHandler) GetDimensions() image.Rectangle {
+	return handler.Image.Bounds()
+}
+
+// NRGBA64ImageHandler struct to handle NRGBA64 Image
 type NRGBA64ImageHandler struct {
-	Image *image.NRGBA64
+	Image     *image.NRGBA64
 	ImageType ImageType
 }
 
@@ -95,12 +101,17 @@ func (handler *NRGBA64ImageHandler) Set(row, column int, px Pixel) error {
 
 // Mode returns the ImageType of the images
 func (handler *NRGBA64ImageHandler) Mode() ImageType {
-	return handler.ImageType;
+	return handler.ImageType
+}
+
+// GetDimensions returns the dimensions as a Rectangle for the image
+func (handler *NRGBA64ImageHandler) GetDimensions() image.Rectangle {
+	return handler.Image.Bounds()
 }
 
 // RGBAImageHandler struct to handle RGBA Image
 type RGBAImageHandler struct {
-	Image *image.RGBA
+	Image     *image.RGBA
 	ImageType ImageType
 }
 
@@ -126,12 +137,17 @@ func (handler *RGBAImageHandler) Set(row, column int, px Pixel) error {
 
 // Mode returns the ImageType of the images
 func (handler *RGBAImageHandler) Mode() ImageType {
-	return handler.ImageType;
+	return handler.ImageType
+}
+
+// GetDimensions returns the dimensions as a Rectangle for the image
+func (handler *RGBAImageHandler) GetDimensions() image.Rectangle {
+	return handler.Image.Bounds()
 }
 
 // RGBA64ImageHandler struct to handle the RGBA64 Image
 type RGBA64ImageHandler struct {
-	Image *image.RGBA64
+	Image     *image.RGBA64
 	ImageType ImageType
 }
 
@@ -157,12 +173,17 @@ func (handler *RGBA64ImageHandler) Set(row, column int, px Pixel) error {
 
 // Mode returns the ImageType of the images
 func (handler *RGBA64ImageHandler) Mode() ImageType {
-	return handler.ImageType;
+	return handler.ImageType
+}
+
+// GetDimensions returns the dimensions as a Rectangle for the image
+func (handler *RGBA64ImageHandler) GetDimensions() image.Rectangle {
+	return handler.Image.Bounds()
 }
 
 // GrayImageHandler struct to handle the Gray Image
 type GrayImageHandler struct {
-	Image *image.Gray
+	Image     *image.Gray
 	ImageType ImageType
 }
 
@@ -188,12 +209,17 @@ func (handler *GrayImageHandler) Set(row, column int, px Pixel) error {
 
 // Mode returns the ImageType of the images
 func (handler *GrayImageHandler) Mode() ImageType {
-	return handler.ImageType;
+	return handler.ImageType
+}
+
+// GetDimensions returns the dimensions as a Rectangle for the image
+func (handler *GrayImageHandler) GetDimensions() image.Rectangle {
+	return handler.Image.Bounds()
 }
 
 // Gray16ImageHandler struct to handle the Gray16 Image
 type Gray16ImageHandler struct {
-	Image *image.Gray16
+	Image     *image.Gray16
 	ImageType ImageType
 }
 
@@ -219,11 +245,16 @@ func (handler *Gray16ImageHandler) Set(row, column int, px Pixel) error {
 
 // Mode returns the ImageType of the images
 func (handler *Gray16ImageHandler) Mode() ImageType {
-	return handler.ImageType;
+	return handler.ImageType
+}
+
+// GetDimensions returns the dimensions as a Rectangle for the image
+func (handler *Gray16ImageHandler) GetDimensions() image.Rectangle {
+	return handler.Image.Bounds()
 }
 
 // ReadImage reads the image from the specified path
-// it will return the interface ImageHandler of the 
+// it will return the interface ImageHandler of the
 // correct type specified in the image
 func ReadImage(path string) (ImageHandler, error) {
 	reader, err := os.Open(path)
@@ -236,43 +267,87 @@ func ReadImage(path string) (ImageHandler, error) {
 		return nil, err
 	}
 
-	switch imgT := img.(type) {
+	switch img.(type) {
 	case *image.NRGBA:
 		return &NRGBAImageHandler{
-			Image: img.(*image.NRGBA),
+			Image:     img.(*image.NRGBA),
 			ImageType: ImageTypeNRGBA,
 		}, nil
 
 	case *image.NRGBA64:
 		return &NRGBA64ImageHandler{
-			Image: img.(*image.NRGBA64),
+			Image:     img.(*image.NRGBA64),
 			ImageType: ImageTypeNRGBA64,
 		}, nil
 
 	case *image.RGBA:
 		return &RGBAImageHandler{
-			Image: img.(*image.RGBA),
+			Image:     img.(*image.RGBA),
 			ImageType: ImageTypeRGBA,
 		}, nil
 
 	case *image.RGBA64:
 		return &RGBA64ImageHandler{
-			Image: img.(*image.RGBA64),
+			Image:     img.(*image.RGBA64),
 			ImageType: ImageTypeRGBA64,
 		}, nil
 
 	case *image.Gray:
 		return &GrayImageHandler{
-			Image: img.(*image.Gray),
+			Image:     img.(*image.Gray),
 			ImageType: ImageTypeGray,
 		}, nil
 
 	case *image.Gray16:
 		return &Gray16ImageHandler{
-			Image: img.(*image.Gray16),
+			Image:     img.(*image.Gray16),
 			ImageType: ImageTypeGray16,
 		}, nil
 
+	default:
+		return nil, ErrUnsupportedImageFormat
+	}
+}
+
+// New returns an ImageHandler with a new Image
+// This handler will be used for the result
+func New(imgT ImageType, dimensions image.Rectangle) (ImageHandler, error) {
+	switch imgT {
+	case ImageTypeNRGBA:
+		return &NRGBAImageHandler{
+			Image:     image.NewNRGBA(dimensions),
+			ImageType: ImageTypeNRGBA,
+		}, nil
+
+	case ImageTypeNRGBA64:
+		return &NRGBA64ImageHandler{
+			Image:     image.NewNRGBA64(dimensions),
+			ImageType: ImageTypeNRGBA64,
+		}, nil
+
+	case ImageTypeRGBA:
+		return &RGBAImageHandler{
+			Image:     image.NewRGBA(dimensions),
+			ImageType: ImageTypeRGBA,
+		}, nil
+
+	case ImageTypeRGBA64:
+		return &RGBA64ImageHandler{
+			Image:     image.NewRGBA64(dimensions),
+			ImageType: ImageTypeRGBA64,
+		}, nil
+
+	case ImageTypeGray:
+		return &GrayImageHandler{
+			Image:     image.NewGray(dimensions),
+			ImageType: ImageTypeGray,
+		}, nil
+
+	case ImageTypeGray16:
+		return &Gray16ImageHandler{
+			Image:     image.NewGray16(dimensions),
+			ImageType: ImageTypeGray16,
+		}, nil
 	default:
 		return nil, ErrUnsupportedImageFormat
 	}
