a7b28a4509a89f0aa76d295d1aefedca837f59fc
reficul31 <reficul31@gmail.com>
2021-07-05 20:10:04 +0530

    Adding convolution of the filter code

--- convolution.go
+++ convolution.go
@@ -8,5 +8,38 @@ func Apply(filter *Kernel, src ImageHandler) (ImageHandler, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	xShift, yShift := filter.CalculateShift()
+	for i := src.GetDimensions().Min.X; i < src.GetDimensions().Max.X-int(filter.Rows); i = i + 1 {
+		for j := src.GetDimensions().Min.Y; j < src.GetDimensions().Max.Y-int(filter.Columns); j = j + 1 {
+
+			rSum, gSum := uint32(0), uint32(0)
+			bSum := uint32(0)
+			for row := 0; row < int(filter.Rows); row = row + 1 {
+				for column := 0; column < int(filter.Columns); column = column + 1 {
+					pixel, err := src.At(i+row, j+column)
+					if err != nil {
+						return nil, err
+					}
+					multiplier, err := filter.At(uint32(row), uint32(column))
+					if err != nil {
+						return nil, err
+					}
+					rSum = rSum + pixel.r*uint32(multiplier)
+					gSum = gSum + pixel.g*uint32(multiplier)
+					bSum = bSum + pixel.b*uint32(multiplier)
+				}
+			}
+
+			oldPixel, err := src.At(i+xShift, j+yShift)
+			if err != nil {
+				return nil, err
+			}
+
+			newPixel := Pixel{r: rSum / uint32(filter.Sum), g: gSum / uint32(filter.Sum), b: bSum / uint32(filter.Sum), a: oldPixel.a}
+			result.Set(i+xShift, j+yShift, newPixel)
+		}
+	}
+
 	return result, nil
 }
