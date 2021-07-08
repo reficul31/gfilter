e644377c9b2c6785d0796f78b31db28dfab256aa
reficul31 <reficul31@gmail.com>
2021-07-08 22:03:08 +0530

    Adding code for filter sum and fixing row and column data type

--- filter.go
+++ filter.go
@@ -4,23 +4,30 @@ package gfilter
 type Kernel struct {
 	values  []float32
 	Sum     float32
-	Rows    uint32
-	Columns uint32
+	Rows    int
+	Columns int
 }
 
 // CreateKernel creates a kernel object from the params
-func CreateKernel(values []float32, rows, columns uint32) (*Kernel, error) {
+func CreateKernel(values []float32, rows, columns int) (*Kernel, error) {
 	if rows == 0 || columns == 0 {
 		return nil, ErrRowColumnCannotBeZero
 	}
 
-	var sum float32 = 0
+	var totalsum, possum float32 = 0, 0
 	for i := 0; i < len(values); i = i + 1 {
-		sum = sum + values[i]
+		totalsum = totalsum + values[i]
+
+		if values[i] > 0 {
+			possum = possum + values[i]
+		}
 	}
 
-	if sum == 0 {
-		sum = 1
+	var sum float32
+	if sum <= 0 {
+		sum = possum
+	} else {
+		sum = totalsum
 	}
 
 	return &Kernel{
@@ -32,7 +39,7 @@ func CreateKernel(values []float32, rows, columns uint32) (*Kernel, error) {
 }
 
 // At gets the value of the kernel at the specified row and column
-func (k *Kernel) At(row, column uint32) (float32, error) {
+func (k *Kernel) At(row, column int) (float32, error) {
 	if row >= k.Rows || column >= k.Columns {
 		return 0.0, ErrRowColumnOutOfBounds
 	}
