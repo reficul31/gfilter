5ab732b5348b85ca3aa0ede7e5e8f24d6439d440
reficul31 <reficul31@gmail.com>
2021-07-17 20:40:58 +0530

    Fixing type conversion in the convolution code

--- convolution.go
+++ convolution.go
@@ -1,5 +1,7 @@
 package gfilter
 
+import "math"
+
 // Apply is the convolution function used to apply the provided filter
 // on the given image. It makes a copy and returns the filtered image
 // without modifying the src image.
@@ -12,7 +14,7 @@ func Apply(filter *Kernel, src ImageHandler) (ImageHandler, error) {
 	xShift, yShift := filter.CalculateShift()
 	for i := src.GetDimensions().Min.X; i < src.GetDimensions().Max.X-int(filter.Rows); i = i + 1 {
 		for j := src.GetDimensions().Min.Y; j < src.GetDimensions().Max.Y-int(filter.Columns); j = j + 1 {
-			var rSum, gSum, bSum uint32 = 0, 0, 0
+			var rSum, gSum, bSum float64 = 0, 0, 0
 			for row := 0; row < int(filter.Rows); row = row + 1 {
 				for column := 0; column < int(filter.Columns); column = column + 1 {
 					pixel, err := src.At(i+row, j+column)
@@ -23,9 +25,9 @@ func Apply(filter *Kernel, src ImageHandler) (ImageHandler, error) {
 					if err != nil {
 						return nil, err
 					}
-					rSum = rSum + pixel.r*uint32(multiplier)
-					gSum = gSum + pixel.g*uint32(multiplier)
-					bSum = bSum + pixel.b*uint32(multiplier)
+					rSum = rSum + float64(pixel.r)*float64(multiplier)
+					gSum = gSum + float64(pixel.g)*float64(multiplier)
+					bSum = bSum + float64(pixel.b)*float64(multiplier)
 				}
 			}
 
@@ -33,7 +35,7 @@ func Apply(filter *Kernel, src ImageHandler) (ImageHandler, error) {
 			if err != nil {
 				return nil, err
 			}
-			newPixel := Pixel{r: rSum / uint32(filter.Sum), g: gSum / uint32(filter.Sum), b: bSum / uint32(filter.Sum), a: uint32(oldPixel.a)}
+			newPixel := Pixel{r: uint32(math.Max(rSum, 0) / float64(filter.Sum)), g: uint32(math.Max(gSum, 0) / float64(filter.Sum)), b: uint32(math.Max(bSum, 0) / float64(filter.Sum)), a: uint32(oldPixel.a)}
 			result.Set(i+xShift, j+yShift, newPixel)
 		}
 	}
